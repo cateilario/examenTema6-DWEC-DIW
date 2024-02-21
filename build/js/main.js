7c550495cd3a19cf729076859c6c1f2bdf7df132
Cate <caterinailariopaz@alumnos.tuniversformacion.es>
2024-02-21 09:53:34 +0100

    create gallery function done

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,9 @@
 import { getYear } from "./getYear.js";
 import { navFixed } from "./navFixed.js";
-// import { createGallery } from "./createGallery.js";
+import { createGallery } from "./createGallery.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   getYear();
   navFixed();
-  //   createGallery();
+  createGallery();
 });
