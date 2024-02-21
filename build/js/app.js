aaf664560717a50f65e7f1f7d88df900ee1db17e
Cate <caterinailariopaz@alumnos.tuniversformacion.es>
2024-02-21 08:42:02 +0100

    images and videos imported

---
+++ build/js/app.js
@@ -0,0 +1,23 @@
+import { startApp } from "./startApp.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  startApp();
+});
+
+
+
+// function scrollNav(){
+//   const links = document.querySelectorAll(".main-nav a");
+
+//   links.forEach(link => {
+//     link.addEventListener('click', function(e){
+//       e.preventDefault();
+
+//       const sectionScroll = e.target.attributes.href.value;
+
+//       const section = document.querySelector(sectionScroll);
+
+//       section.scrollIntoView({ "behavior": smooth})
+//     });
+//   });
+// }
